3b2e49aae13ede0c78cfadf163092a8b10089647
Justine <j.debelius@gmail.com>
2020-04-22 17:52:18 -0600

    updates __init__.py

--- q2_sidle/__init__.py
+++ q2_sidle/__init__.py
@@ -1,2 +1,4 @@
-
 from ._filter_seqs import filter_degenerate_sequences
+from ._extract import (extract_regional_database, 
+					   prepare_extracted_region)
+from ._kmer_map import (KmerMapFormat, KmerMap, KmerMapsDirFmt)
