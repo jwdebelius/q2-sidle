253c32b2a26ad57c5639c5c4384de8b8b4dd0873
Justine <j.debelius@gmail.com>
2020-04-29 14:14:19 -0600

    fixes import

--- q2_sidle/__init__.py
+++ q2_sidle/__init__.py
@@ -3,5 +3,5 @@ from ._filter_seqs import (filter_degenerate_sequences,
 						   )
 from ._extract import (extract_regional_database, 
 					   prepare_extracted_region)
-from ._kmer_map import (KmerMapFormat, KmerMap, KmerMapsDirFmt, 
+from ._kmer_map import (KmerMapFormat, KmerMap, KmerMapDirFmt, 
 						KmerAlignment, KmerAlignFormat, KmerAlignDirFmt)
