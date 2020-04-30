b7403016c7b8d537478a2bfef0103ca1d9c7006c
Justine <j.debelius@gmail.com>
2020-04-30 14:28:38 -0600

    shifts imports

--- q2_sidle/__init__.py
+++ q2_sidle/__init__.py
@@ -1,7 +1,16 @@
 from ._align import align_regional_kmers
-from ._filter_seqs import (filter_degenerate_sequences,
-						   )
-from ._extract import (extract_regional_database, 
-					   prepare_extracted_region)
-from ._kmer_map import (KmerMapFormat, KmerMap, KmerMapDirFmt, 
-						KmerAlignment, KmerAlignFormat, KmerAlignDirFmt)
+from ._filter_seqs import (filter_degenerate_sequences)
+from ._extract import (extract_regional_database,
+                       prepare_extracted_region)
+from ._reconstruct import reconstruct_counts
+from ._type import (KmerMap,
+                    KmerAlignment,
+                    SidleReconstruction,
+                    ReconstructionSummary,
+                    )
+
+from ._formats import (KmerMapFormat, KmerMapDirFmt,
+                       KmerAlignFormat, KmerAlignDirFmt,
+                       ReconSummaryFormat, ReconSummaryDirFormat,
+                       SidleReconFormat, SidleReconDirFormat,
+                       )
