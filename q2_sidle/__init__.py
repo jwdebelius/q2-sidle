58bfe18cc3d62749673a15cceca2086dc37baf25
Justine <j.debelius@gmail.com>
2021-07-12 12:04:47 -0600

    hides pipeline

--- q2_sidle/__init__.py
+++ q2_sidle/__init__.py
@@ -8,7 +8,7 @@ from ._formats import (KmerMapFormat, KmerMapDirFmt,
                        ReconSummaryFormat, ReconSummaryDirFormat,
                        SidleReconFormat, SidleReconDirFormat,
                        )
-from ._pipelines import (sidle_reconstruction)
+# from ._pipelines import (sidle_reconstruction)
 from ._reconstruct import reconstruct_counts
 from ._taxonomy import reconstruct_taxonomy
 from ._tree import reconstruct_fragment_rep_seqs
